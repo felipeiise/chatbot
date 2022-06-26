96e22086317c4c1d380d0952880c7add38818ae2
Felipe Iise Mendes <felipeiisemendes@gmail.com>
2022-06-26 19:41:43 -0300

    Atualização do mode de lock

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/index.html');
 });
 
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 io.on('connection', (client) => {
 
     client.on('join', function(number, message) {
@@ -27,26 +31,74 @@ io.on('connection', (client) => {
         client.join(number);
     });
 
-    client.on('messages', function(mode, number, message) { // inside every message event we set a timeout
+    client.on('messages', async function(mode, number, message) { // inside every message event we set a timeout
 
-        console.log(`Nova mensagem de ${number}: ${message}`);
+        io.to(number).emit('broad', '<span class="user">' + message + '</span>');
+
+        //console.log(`Nova mensagem de ${number}: ${message}`);
         
         const message_id = uuidEmit();
-        console.log(`Your message UUID v4 is: ${message_id}`);
+        //console.log(`Your message UUID v4 is: ${message_id}`);
 
-        saveMessage(number, message, message_id);
+        if (mode === 'each') {
 
-        let timeout = mode === 'each' ? 0 : RESPONSE_TIME;
+            timeout = 0;
 
-        setTimeout(() => {
-            doReply(mode, number, message_id);
-        }, timeout);
-        
-        io.to(number).emit('broad', '<span class="user">' + message + '</span>');
+            saveMessage(number, message, message_id);
+
+            setTimeout(() => {
+                doReply(mode, number, message_id);
+            }, timeout);
+
+        } else if (mode === 'first' || mode === 'all') {
+
+            timeout = RESPONSE_TIME;
+
+            saveMessage(number, message, message_id);
+
+            setTimeout(() => {
+                doReply(mode, number, message_id);
+            }, timeout);
+
+        } else if (mode === 'lock') {
+            
+            console.log('\nlock mode\n');
+
+            timeout = randomInteger(2500, 5000);
+
+            const isLocked = await getLock(number);
+
+            if (isLocked === 'true') {
+                console.log('Mensagem descartada até a primeira mensagem ser respondida');
+            } else {
+                saveMessage(number, message, message_id);
+                setLock(number, true);
+                console.log('Primeira mensagem');
+
+                setTimeout(() => {
+                    doReply(mode, number, message_id);
+                }, timeout);
+            }
+
+        }
+
+        console.log(timeout);
 
     });
 
 });
+
+async function getLock(number) {
+    return await redis.get(`lock_${number}`).then((result) => {
+        console.log('getLock: ' + result);
+        return result;
+    });
+}
+
+async function setLock(number, status) {
+    console.log('setLock');
+    await redis.set(`lock_${number}`, status);
+}
 
 async function saveMessage(number, message, message_id) {
     await redis.set(`reply_${number}`, message_id);
@@ -58,10 +110,15 @@ async function doReply(mode, number, message_id) {
 
     let reply = await redis.get(`reply_${number}`);
 
+    let lock = await redis.get(`lock_${number}`);
+
     if (reply != 0) {
         if (mode === 'first' || mode === 'each') {
             getAndReplyFirst(number, message_id);
-        } else {
+        } else if (mode === 'lock') {
+            await getAndReplyLocked(number, message_id);
+            await setLock(number, false);
+        } else if (mode === 'all') {
             getAndReplyAll(number);
         }
     } else {
@@ -70,27 +127,57 @@ async function doReply(mode, number, message_id) {
 
 }
 
-async function getAndReplyFirst(number, message_id) {
-
-    console.log('ReplyFirst');
-
+async function getAndReplyLocked(number, message_id) {
+    console.log('ReplyLocked');
     console.log(`message_id: ${message_id}`);
-    
-    let message = await redis.lindex(number, -1); // get first message from list
+
+    // recupero a primeira mensagem da lista de mensagens do usuário
+    // deleto a lista nas linhas abaixo
+    // e libero o chatbot para responder a primeira mensagem novamente
+    let message = await redis.lindex(number, -1);
 
     const [id, ...text] = message.split(':');
 
     console.log(`id: ${id}`);
     console.log(`text: ${text}`);
 
-    if (id === message_id) {
+    if (id === message_id) { //somente responde a primeira mensagem da lista acima
 
         io.to(number).emit('broad', `
             <span class="reply">Você: <b>${text}</b></span>
             <span class="bot">Resposta da mensagem: <b>${text}</b></span>
         `);
 
-        redis.del(number);
+        redis.del(number); // descarta o restante das mensagens
+        //await redis.set(`lock_${number}`, false);
+
+    }
+}
+
+async function getAndReplyFirst(number, message_id) {
+
+    console.log('ReplyFirst');
+
+    console.log(`message_id: ${message_id}`);
+    
+    // recupero a primeira mensagem da lista de mensagens do usuário
+    // deleto a lista nas linhas abaixo
+    // e libero o chatbot para responder a primeira mensagem novamente
+    let message = await redis.lindex(number, -1);
+
+    const [id, ...text] = message.split(':');
+
+    console.log(`id: ${id}`);
+    console.log(`text: ${text}`);
+
+    if (id === message_id) { //somente responde a primeira mensagem da lista acima
+
+        io.to(number).emit('broad', `
+            <span class="reply">Você: <b>${text}</b></span>
+            <span class="bot">Resposta da mensagem: <b>${text}</b></span>
+        `);
+
+        redis.del(number); // descarta o restante das mensagens
         redis.set(`reply_${number}`, 0);
 
     }
@@ -106,6 +193,7 @@ async function getAndReplyAll(number) {
 
         console.log('replyAll');
 
+        // recupero as mensagens do usuário e deleto a lista
         let messages = await redis.lrange(number, 0, -1);
         redis.del(number);
 
